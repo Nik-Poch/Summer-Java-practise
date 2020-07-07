2e4fc9c61af74981f002d797d8bee599a0a43ae7
FireRalin <42908420+FireRalin@users.noreply.github.com>
2020-07-08 00:14:51 +0300

    Fast fix
    Исправил несколько недочетов, добавил комментов и проверок

--- Miner/src/main/java/etu/leti/generator/Generator.java
+++ Miner/src/main/java/etu/leti/generator/Generator.java
@@ -40,7 +40,7 @@ public class Generator
 
         for (int i = 0; i<4; i++)
         {
-            rand = random.nextInt(100 ) + 1;/**Рандомим 1 или 2 руды с вероятностью 70% в большую сторону*/
+            rand = random.nextInt(100 ) + 1;/**Рандомим 1 или 2 жилы в каждой четверти с вероятностью 70% в бОльшую сторону*/
             if (rand>30)
             {
                 count_veins_in_sector = 2;
@@ -59,27 +59,34 @@ public class Generator
 
             for (int n = 0; n<count_veins_in_sector;n++)
             {
-                rand_x = random.nextInt((x / 2) + curr_x) + curr_x;/**Радномим точку спауна руды*/
-                rand_y = random.nextInt((x / 2) + curr_y) + curr_y;
-                rand_ore =  random.nextInt(4) + 1;/**Радномим тип руды*/
+                rand_x = random.nextInt((x / 2)-1) + curr_x;/**Радномим точку спауна руды*/
+                rand_y = random.nextInt((x / 2)-1) + curr_y;
+                rand_ore =  random.nextInt(2) + 1;/**Радномим тип руды*/
                 rand_size = random.nextInt(4) + random.nextInt(3) + 1;/**Радномим размер жилы нормальный распределением (почти) */
-                for(int g = 0;g<10;g++)
+                for(int g = 0;g<10;g++)/**Даем 10 попыток попасть "сердцем" жилы в пустое место*/
                 {
                     if (field[rand_x][rand_y] == null)
                     {
                         g=10;
                         field[rand_x][rand_y] = new Cell(rand_x, rand_y, new Ore(picture.get(rand_ore), ore.get(rand_ore)));/**Ставим первую руду*/
-                        for (int k = 0; k < rand_size; k++) {
+                        for (int k = 0; k < rand_size; k++)
+                        {
                             int vein_x = rand_x, vein_y = rand_y;/**Запоминаем точку, где поставили*/
                             rand_x = random.nextInt(2) - 1;/**Радномим Отклонение*/
-                            if (rand_x == 0) {
-                                rand_y = random.nextInt(2) - 1;
-                            } else
-                                rand_y = 0;
-                            if (field[vein_x + rand_x][vein_y + rand_y] == null)/**Проверка на то, что не попали в другую жилу*/ {
-                                vein_x += rand_x;/**Сменяем последнюю координату жилы*/
-                                vein_y += rand_y;
-                                field[vein_x][vein_y] = new Cell(vein_x, vein_y, new Ore(picture.get(rand_ore), ore.get(rand_ore)));/**Ставим блок*/
+                            if (rand_x == 0) /**если по х отклонение 0, то смотрим у*/
+                            {
+                                rand_y = random.nextInt(2) - 1;/**существует вероятность два раза получить 0, 1\9 примерно, можно поправить, но пока забил*/
+                            }
+                            else
+                                rand_y = 0;/**если по х УЖЕ есть отклонение, то у делаем 0, так как по-диагонали нельзя ставить руду*/
+                            if (vein_x + rand_x<x && vein_y + rand_y<y)
+                            {
+                                if (field[vein_x + rand_x][vein_y + rand_y] == null)/**Проверка на то, что не попали в другую жилу*/
+                                {
+                                    vein_x += rand_x;/**Сменяем последнюю координату жилы*/
+                                    vein_y += rand_y;
+                                    field[vein_x][vein_y] = new Cell(vein_x, vein_y, new Ore(picture.get(rand_ore), ore.get(rand_ore)));/**Ставим блок*/
+                                }
                             }
                         }
                     }
