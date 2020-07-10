e5e2ed0603b0910334d0bfa712d9566b5687b911
FireRalin <42908420+FireRalin@users.noreply.github.com>
2020-07-10 22:52:34 +0300

    save beforge merge with 16

--- Miner/src/main/java/etu/leti/algorithmTest/AlgorithmTest.java
+++ Miner/src/main/java/etu/leti/algorithmTest/AlgorithmTest.java
@@ -10,17 +10,23 @@ import etu.leti.ore.Ore;
 import etu.leti.ore.OreTypes;
 import etu.leti.algorithm.Graph;
 import etu.leti.field.Field;
+import etu.leti.gui.FieldVisualizer;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class AlgorithmTest {
+    private static final int fieldWidth = 20;
+    private static final int fieldHeight = 21;
     private Graph graph;
     private Cell[] cellMap;
+    private Cell[][] convertedArray;
     private Field testMap;
     private MapParser mapParser;
+    private ArrayList<Cell> result;
 
 
     @Before
@@ -33,6 +39,16 @@ public class AlgorithmTest {
         } catch (FileNotFoundException exc) {
             return;
         }
-
+        convertedArray = FieldVisualizer.convertArrayForAlgorithm(FieldVisualizer.convert1DArrayTo2D(cellMap, fieldWidth ,fieldHeight));
+        graph = new Graph(new Field(convertedArray, fieldHeight,fieldWidth ));
+        result = graph.getCellsOfShortestWay();
     }
+
+    @Test
+    public void algorithmPathLength(){
+        double actualLength = 20;
+        double expectedLength = graph.getLengthMinWay();
+        Assert.assertEquals(actualLength, actualLength);
+    }
+
 }
